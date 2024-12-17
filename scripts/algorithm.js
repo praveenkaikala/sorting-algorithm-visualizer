24c5e67ac85b0db15b719b3e35d0be06246f5aac
praveenkaikala <142200525+praveenkaikala@users.noreply.github.com>
2024-12-17 10:05:53 +0530

    creating algo class

--- scripts/algorithm.js
+++ scripts/algorithm.js
@@ -1,1 +1,14 @@
-console.log("algoritm")
+"use strict"
+
+class Algorithms{
+    constructor(time)
+    {
+        this.list=document.querySelectorAll(".cell")
+        this.length=list.length;
+        this.time=time;
+        this.help=new Helper(this.time,this.list)
+    }
+    BubbleSort=async()=>{
+        
+    }
+}
